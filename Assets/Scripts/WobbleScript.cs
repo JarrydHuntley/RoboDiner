09753cc1ceb114063524b2cd1ebdfa5b46313ec5
Jarryd Huntley <Jarryd.huntley@gmail.com>
2018-05-06 00:01:42 -0400

    Completed the wobble script.

--- Assets/Scripts/WobbleScript.cs
+++ Assets/Scripts/WobbleScript.cs
@@ -6,44 +6,35 @@ public class WobbleScript : MonoBehaviour
 {
 
     private SpriteRenderer sr;
+    [SerializeField]
     private Sprite[] sprites;
 
-    private bool useTimingOffset = false;
+    [SerializeField]
     private float frameSpeed = 0.5f;
-   // private float frameToggleSpeed = 0.5f;
-
     private float _frameTimer = 0.0f;
-   // private float _frameToggle = 0.0f;
+
 
     private int currentFrame;
-   // private bool secondFrame;
-
-    //private int frameCount;
 
     // Use this for initialization
     void Start()
     {
-        GetComponent<SpriteRenderer>();
-        //frameCount = sprites.Length / 2;
+        sr = GetComponent<SpriteRenderer>();
+        _frameTimer += Random.Range(0.1f , 1.8f);
     }
 
     // Update is called once per frame
     void Update()
     {
         _frameTimer -= Time.deltaTime;
-        //_frameToggle -= Time.deltaTime;   
 
         //counting frames
         if (Mathf.Max(0.0f, _frameTimer) == 0.0f)
         {
             _frameTimer = frameSpeed;
             currentFrame = (currentFrame + 1) % sprites.Length;
+            Debug.Log(sprites[currentFrame]);
+            sr.sprite = sprites[currentFrame];
         }
-
-        //toggling frames
-        //if (Mathf.Max(0.0f, _frameToggle) == 0.0f)
-        //{
-        //    _frameToggle = frameToggleSpeed;
-        //}
     }
 }
